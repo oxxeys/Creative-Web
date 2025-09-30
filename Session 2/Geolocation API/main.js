c5f5124fc3ad2d049983ea3e5348b52075dfa608
oxxeys <147168632+oxxeys@users.noreply.github.com>
2025-09-30 12:08:59 +0100

    Session 3

--- Session 2/Geolocation API/main.js
+++ Session 2/Geolocation API/main.js
@@ -1,5 +1,9 @@
 const TextBox = document.getElementById("location")
 document.getElementById("btn").addEventListener("click", getLocation)
+const SunTimes = document.getElementById("SunTimes")
+
+let lat
+let long
 
 function getLocation(){
     if (navigator.geolocation){
@@ -12,8 +16,22 @@ function getLocation(){
 
 function success(position){
     TextBox.innerHTML = "Latitude : " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
 }
 
 function error() {
   alert("Sorry, no position available.");
+}
+
+function getSunTime(){
+  let urlBase="https://api.sunrise-sunset.org/json?"
+  let queryURL=urlBase+"lat="+lat+"&lng="+long
+  fetch(queryURL)
+    .then(data=>data.json())
+    .then(processSunTimes)
+}
+
+function processSunTimes(sunData){
+  SunTimes.innerText="sunrise: "+ sunData.results.sunrise
 }
