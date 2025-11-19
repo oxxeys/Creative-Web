1e03313bcff43b681dda49df70031045bfd0846b
oxxeys <147168632+oxxeys@users.noreply.github.com>
2025-11-19 13:36:40 +0000

    Work From Home

--- simpleSocialStarterEJS/index.js
+++ simpleSocialStarterEJS/index.js
@@ -1,6 +1,6 @@
-const express= require('express')
-const app=express()
-const path=require('path')
+const express = require('express')
+const app = express()
+const path = require('path')
 
 const posts = require("./models/posts.js")
 const userModel = require("./models/users.js")
@@ -9,19 +9,18 @@ const { request } = require('http')
 const req = require('express/lib/request.js')
 
 const sessions = require("express-session")
-const cookieParser = require("cookie-parser") 
+const cookieParser = require("cookie-parser")
 
-const threeMiniutes = 3*60*1000
-const oneHour = 1*60*60*1000
+const threeMiniutes = 3 * 60 * 1000
+const oneHour = 1 * 60 * 60 * 1000
 
 const dotenv = require('dotenv').config()
-const mongoDBUsername=process.env.mongoDBUsername
-const mongoDBPassword=process.env.mongoDBPassword
-const mongoAppName=process.env.mongoAppName
-console.log(mongoDBUsername, mongoDBPassword, mongoAppName )
+const mongoDBUsername = process.env.mongoDBUsername
+const mongoDBPassword = process.env.mongoDBPassword
+const mongoAppName = process.env.mongoAppName
+console.log(mongoDBUsername, mongoDBPassword, mongoAppName)
 
-const connectionString = `mongodb+srv://${mongoDBUsername}:${mongoDBPassword}@cluster.lpfnqqx.mongodb.net/${mongoAppName}?retryWrites=true&w=majority`
-
+const connectionString = `mongodb+srv://${mongoDBUsername}:${mongoDBPassword}@creativeweb.rcrohxp.mongodb.net/${mongoAppName}?retryWrites=true&w=majority`
 
 const mongoose = require("mongoose")
 mongoose.connect(connectionString)
@@ -29,103 +28,128 @@ mongoose.connect(connectionString)
 
 app.use(sessions({
     secret: "SEcRET CODE", // this is typically stroed in the .env file
-    cookie:{maxAge:threeMiniutes} , 
+    cookie: { maxAge: threeMiniutes },
     resave: false,
     saveUninitialized: false
 }))
 
 
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('listening on port 3000')
 })
 
 app.use(express.static('public'))
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
-function checkLoggedIn(request, response, nextAction){
-    if(request.session){
-        if(request.session.username){
+function checkLoggedIn(request, response, nextAction) {
+    if (request.session) {
+        if (request.session.username) {
             nextAction()
-        }else{
+        } else {
             request.session.destroy()
-            response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
+            // response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
+            response.render("pages/notloggedin", { isLoggedIn: getLoggedStatus(request) })
         }
     }
-    else{
+    else {
         request.session.destroy()
-        response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
+        // response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
+        response.render("pages/notloggedin", { isLoggedIn: getLoggedStatus(request) })
     }
 }
 
-function getLoggedStatus(request){
+function getLoggedStatus(request) {
     return request.session && request.session.username
 }
 
-app.get('/app', checkLoggedIn, async (request, response)=>{
+app.get('/app', checkLoggedIn, async (request, response) => {
 
     response.render("pages/app",
-        {isLoggedIn: getLoggedStatus(request),
-        posts: await posts.getLatestNPosts(8)}
+        {
+            isLoggedIn: getLoggedStatus(request),
+            username: request.session.username,
+            posts: await posts.getLatestNPosts(8)
+        }
     )
 })
 
-app.get('/profile',checkLoggedIn, (request, response)=>{  
-    response.sendFile(path.join(__dirname, '/views', 'profile.html'))
+app.get('/index', checkLoggedIn, (request, response) => {
+    response.render("pages/index", { isLoggedIn: getLoggedStatus(request) })
 })
 
-app.get('/login', (request, response)=>{
+app.get('/profile', checkLoggedIn, (request, response) => {
+    //response.sendFile(path.join(__dirname, '/views', 'profile.html'))
+    response.render("pages/profile", { isLoggedIn: getLoggedStatus(request) })
+})
+
+app.get('/login', (request, response) => {
     // response.sendFile(path.join(__dirname, '/views', 'login.html'))
-    response.render("pages/login",{isLoggedIn: getLoggedStatus(request)})
+    response.render("pages/login", { isLoggedIn: getLoggedStatus(request) })
 })
 
-app.get('/register', (request, response)=>{
-    response.sendFile(path.join(__dirname, '/views', 'register.html'))
+app.get('/register', (request, response) => {
+    // response.sendFile(path.join(__dirname, '/views', 'register.html'))
+    response.render("pages/register", { isLoggedIn: getLoggedStatus(request) })
 })
 
-app.get('/logout', (request, response)=>{
-    response.sendFile(path.join(__dirname, '/views', 'logout.html'))
+app.get('/logout', (request, response) => {
+    // response.sendFile(path.join(__dirname, '/views', 'logout.html'))
+    response.render("pages/logout", { isLoggedIn: getLoggedStatus(request) })
 })
 
-app.post('/logout', (request, response)=>{
-    
+app.post('/logout', (request, response) => {
+
     request.session.destroy()
     response.redirect("/")
 })
 
 
-app.get('/getposts', async (request, response)=>{
-    response.json({posts: await posts.getLatestNPosts(8)}) // dont forget to prefix as it is required from elsewhere
+app.get('/getposts', async (request, response) => {
+    response.json({ posts: await posts.getLatestNPosts(8) }) // dont forget to prefix as it is required from elsewhere
 
 })
 
-app.post("/newpost", (request, response)=>{
-    posts.addPost(request.body.message, request.session.username)
+app.post("/newpost", async (request, response) => {
+    await posts.addPost(request.body.message, request.session.username)
     console.log(request.session.username)
-    response.render("pages/app",{isLoggedIn: getLoggedStatus(request)})
+    const latestPosts = await posts.getLatestNPosts(8)
+    response.render("pages/app",
+        {
+            isLoggedIn: getLoggedStatus(request),
+            username: request.session.username,
+            posts: latestPosts
+        }
+    )
+})
+
+app.post("/postliked", async (request, response) => {
+    console.log("add a like")
 })
 
 
-
-app.post('/login', async (request, response)=>{
-    if(await userModel.checkUser(request.body.username, request.body.password)){
-        request.session.username=request.body.username
+app.post('/login', async (request, response) => {
+    if (await userModel.checkUser(request.body.username, request.body.password)) {
+        request.session.username = request.body.username
         // response.render("pages/app",{isLoggedIn: getLoggedStatus(request)})
-         response.render("pages/app",
-        {isLoggedIn: getLoggedStatus(request),
-        posts: await posts.getLatestNPosts(8)}
-    )
-    } else{
-        response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
-        
+        response.render("pages/app",
+            {
+                isLoggedIn: getLoggedStatus(request),
+                username: request.session.username,
+                posts: await posts.getLatestNPosts(8)
+            }
+        )
+    } else {
+        response.render("pages/notloggedin", { isLoggedIn: getLoggedStatus(request) })
+
     }
 })
 
-app.get('/getuser', (request, response)=>{
-    response.json({user:users.getUser()})
+app.get('/getuser', (request, response) => {
+    response.json({ user: users.getUser() })
 })
 
 // app.post('/register', (request, response)=>{
@@ -147,8 +171,9 @@ app.post('/register', async (request, response) => {
     const success = await userModel.addUser(request.body.username, request.body.password);
     if (success) {
         // response.sendFile(path.join(__dirname, '/views', 'app.html'));
-        response.render("pages/app",{isLoggedIn: getLoggedStatus(request)})
+        response.render("pages/app", { isLoggedIn: getLoggedStatus(request) })
     } else {
-        response.sendFile(path.join(__dirname, '/views', 'registration_failed.html'));
+        // response.sendFile(path.join(__dirname, '/views', 'registration_failed.html'))
+        response.render("pages/notloggedin", { isLoggedIn: getLoggedStatus(request) })
     }
 });
--- simpleSocialStarterEJS/models/posts.js
+++ simpleSocialStarterEJS/models/posts.js
@@ -64,6 +64,7 @@ function addPost(message, user){
 }
 
 
+
 module.exports={
     getPosts,
     addPost,
