2b67aa6d742880f63e29959c9ec04f7d8425682d
oxxeys <147168632+oxxeys@users.noreply.github.com>
2025-11-25 11:55:00 +0000

    Testing

---
+++ EJS-Test/NuxtTest/nuxt-app/app/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Nuxt + Nitro Backend Example!</h1>
+
+    <p v-if="pending">Loading...</p>
+    <p v-else-if="error">Error: {{ error.message }}</p>
+    <p v-else>Message from backend: {{ data.message }}</p>
+  </div>
+</template>
+
+<script setup>
+const { data, pending, error } = await useFetch('/api/hello')
+// call backend
+</script>
+
+<style>
+body {
+  padding: 20px;
+}
+</style>
